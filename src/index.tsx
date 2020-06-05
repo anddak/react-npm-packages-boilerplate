43d97cdfb9d3f5947135fce0a7d37be4f334155d
anddak <andras.dako@gmail.com>
2020-06-05 20:17:13 +0100

    Changed to functional component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,9 @@
-/**
- * @class ExampleComponent
- */
-
 import * as React from "react";
 
-export type Props = { text: string };
-
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const { text } = this.props;
-
-    return <div style={{ color: "red" }}>Hello {text}</div>;
-  }
+export const ExampleComponent = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
 }
